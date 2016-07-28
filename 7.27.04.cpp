40cd12f28a4d855f926a591128595c2f24f4dbdf
CentOS7 <apktoolOo@gmail.com>
2016-07-28 15:09:58 +0800

    static

--- 7.27.04.cpp
+++ 7.27.04.cpp
@@ -14,7 +14,7 @@ class CountedObject{
 		CountedObject();
 		~CountedObject();
 		static int cnt;		//静态成员的引用性声明，对于int和char型的，可以直接在引用性声明这里完成初始化操作，但其他类型不可以
-		static const int num; //对于static const的话，可以不需要“静态成员的定义性声明”，比较特殊
+		static const int num=0; //对于static const的话，可以不需要“静态成员的定义性声明”，比较特殊
 };
 
 int CountedObject::cnt=0;	//静态成员的定义性声明
@@ -22,6 +22,8 @@ int CountedObject::cnt=0;	//静态成员的定义性声明
 int main(int argc, char* argv[]){
 	cout<<CountedObject::cnt<<endl;
 	CountedObject obj1;
+	//cout<<obj1.cnt<<"||"<<obj1.num<<endl;	//使用使用（“对象.静态成员”）的方式访问静态成员，但是不建议。
+											//因为静态成员是属于该类的，如果采用这种方式访问的话，会导致程序可读性变差
 	cout<<CountedObject::cnt<<endl;
 	CountedObject* ptr=new CountedObject;
 	cout<<CountedObject::cnt<<endl;
