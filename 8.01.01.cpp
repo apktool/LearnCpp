59375845ca4773f02df6b3b06cb62f0b74cfa7f7
CentOS7 <apktoolOo@gmail.com>
2016-08-08 22:39:19 +0800

    单例模式

--- 8.01.01.cpp
+++ 8.01.01.cpp
@@ -8,17 +8,30 @@
 using std::cout;
 using std::endl;
 
+//C++11标准 拷贝控制的实现
+class Singleton{
+	public:
+		Singleton();
+		~Singleton();
+		Singleton(const Singleton& obj)=delete;
+		Singleton& operator= (const Singleton& obj)=delete;
+		static Singleton* GetInstance();
+	private:
+		static Singleton* m_instance;
+};
+
+/*
 class Singleton{
 	public:
 		static Singleton* GetInstance();
 		~Singleton();
 	private:
-		Singleton(const Singleton& obj);				/*将拷贝构造函数声明为私有，可以有效防止对象的拷贝*/
-		Singleton& operator= (const Singleton& obj);	/*将等号运算符重载声明为私有，可以防止对象的拷贝*/
+		Singleton(const Singleton& obj);				//将拷贝构造函数声明为私有，可以有效防止对象的拷贝
+		Singleton& operator= (const Singleton& obj);	//将等号运算符重载声明为私有，可以防止对象的拷贝
 		Singleton();
 		static Singleton* m_instance;
 };
-
+*/
 Singleton* Singleton::m_instance;
 
 int main(int argc, char* argv[]){
