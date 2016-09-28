5243475f789023aac2bb1ed8aac021ec846a4c85
CentOS7 <apktoolOo@gmail.com>
2016-09-28 09:29:16 +0800

    内存泄露检测

--- 9.28.01.cpp
+++ 9.28.01.cpp
@@ -19,3 +19,12 @@ int main(int argc, char* argv[]){
 /*
  * g++ 9.28.01.cpp debug_new.cpp -o a.out -Wall -std=c++11
  */
+/*
+ * 检测内存泄露：
+ * 		自己实现malloc，再里面进行跟踪
+ * 第三方工具：
+ * 		Linux：valgrind，dmalloc，efence
+ * 		windows：visual leak detector
+ * 自己实现内存工具：
+ * 		自己重载operator new 和 operator delete 实现追踪
+ */
