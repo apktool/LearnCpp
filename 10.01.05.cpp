ac4f115109d715a2e3a16c43ce5e6ac14cee2b97
CentOS7 <apktoolOo@gmail.com>
2016-10-01 18:32:41 +0800

    通过操纵子方式进行格式化输出

--- 10.01.05.cpp
+++ 10.01.05.cpp
@@ -20,7 +20,7 @@ int main(int argc,char* argv[]){
 
 	cout<<"=================对齐控制=================="<<endl;
 	cout<<setw(10)<<setiosflags(ios::left)<<n<<'#'<<endl;
-	cout<<setw(10)<<n<<'#'<<endl;
+	cout<<setw(10)<<n<<'#'<<endl;//对齐控制，会影响下一个输出
 	cout<<setw(10)<<resetiosflags(ios::left)<<n<<'#'<<endl;
 
 	cout<<"=================填充控制=================="<<endl;
@@ -29,7 +29,7 @@ int main(int argc,char* argv[]){
 	cout<<setw(10)<<setfill(' ')<<n<<endl;
 
 	cout<<"=================精度控制=================="<<endl;
-	cout<<setprecision(4)<<d<<endl;//保留4为有效数字
+	cout<<setprecision(4)<<d<<endl;//保留4位有效数字
 	cout<<setprecision(2)<<d2<<endl;
 
 	cout<<setiosflags(ios::fixed);
