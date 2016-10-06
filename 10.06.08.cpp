30d0fb00620727356a34ce83e612101946a81822
CentOS7 <apktoolOo@gmail.com>
2016-10-06 15:08:36 +0800

    IO流迭代器|ostream_iterator,istream_iterator

--- 10.06.08.cpp
+++ 10.06.08.cpp
@@ -1,6 +1,6 @@
 /**
  * @file 10.06.08.cpp
- * @brief IO流迭代器|ostream_iterator
+ * @brief IO流迭代器|ostream_iterator,istream_iterator
  * @author LiWenGang
  * @date 2016-10-06
  */
@@ -16,6 +16,11 @@ int main(int argc, char* argv[]){
 	vector<int> v(a,a+5);
 
 	copy(v.begin(),v.end(),ostream_iterator<int>(cout," "));
+	cout<<endl;
+
+	vector<int> v2;
+	copy(istream_iterator<int>(cin),istream_iterator<int>(),back_inserter(v2));
+	copy(v2.begin(),v2.end(),ostream_iterator<int>(cout," "));
 	cout<<endl;
 	return 0;
 }
